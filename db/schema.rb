fe0ab6cae268fdd007fff5a4e2f4eb7caccb805c
Sri Prasanna <k.sriprasanna@gmail.com>
2014-06-04 21:05:07 -0500

    adding dummy scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140605020442) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
