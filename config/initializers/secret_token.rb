4b4dedd3ca6ca95061176d0f460078fd05ca232f
Sri Prasanna <k.sriprasanna@gmail.com>
2014-06-04 21:00:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PuppetRailsDemo::Application.config.secret_key_base = '31581fdcd2714e0fe3175a66db9f4f8adef381f1afa978aa8137eedd383c16c1611bfd26acc44c4750784d77b36691741f3eba513256626f53a01f5a78ca95e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PuppetRailsDemo::Application.config.session_store :cookie_store, key: '_puppet-rails-demo_session'
